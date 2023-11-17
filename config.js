c4747c6d9688a1fd1b738604cb507539e3530d96
Ivan-Malakhovskyi <124193519+Ivan-Malakhovskyi@users.noreply.github.com>
2023-11-17 15:52:34 +0200

    add config.js

---
+++ config.js
@@ -0,0 +1,2 @@
+export const DB_HOST =
+  "mongodb+srv://testUser123-:zK8igxDpYBdOCA2j@cluster0.tqiviet.mongodb.net/db-contacts?retryWrites=true&w=majority";
--- server.js
+++ server.js
@@ -1,28 +1,64 @@
 import { app } from "./app.js";
+import mongoose from "mongoose";
+import { DB_HOST } from "./config.js";
 
-app.listen(3000, () =>
-  console.log("Server running. Use our API on port: 3000")
+const { Schema } = mongoose;
+
+const contactsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      minlength: 2,
+      maxlength: 7,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "Email is required"],
+    },
+    phone: {
+      type: Number,
+      min: 2,
+      max: 10,
+      enum: ["Recent", "Blocked"],
+      match: /^\d{4}$/,
+      required: [true, "Phone is required"],
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
 );
 
-//* ==============================================
+// contactsSchema.index({ name: 1 });
 
-// app.use((req, res, next) => {
-//   console.log("object");
-// }); //* middleware для кожного запиту
+// const Contact = mongoose.model("Contact", contactsSchema);
 
-//! Використання middleware
-// ? CORS
-
-// app.get("/", (req, res) => {
-//   res.send("<h1>This is Main PAge</h1>");
+// const contact = new Contact({
+//   phone: String,
+//   email: String,
 // });
 
-// app.get("/contacts", (req, res) => {
-//   console.log(req.url);
-//   console.log(req.method);
-//   res.send("This is contacts Page");
-// });
+// contact.methods.fullInfo = function name() {
+//   return `${this.phone} ${this.email}`;
+// };
 
-// app.listen(3000, () =>
-//   console.log("Server running. Use our API on port: 3000")
-// );
+//zK8igxDpYBdOCA2j
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("✔ Success conecting");
+    app.listen(3000, () =>
+      console.log("Server running. Use our API on port: 3000")
+    );
+  })
+  .catch((err) => {
+    console.log("❌ ooops...", err.message);
+    process.exit(1); //*закриття всіх запущених процесів
+  });
+
+// console.log(process.env);
